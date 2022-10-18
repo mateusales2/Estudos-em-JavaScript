079f81eb9cd3cb6d94da165ff773e29cf474d655
Mateusales2 <114072680+mateusales2@users.noreply.github.com>
2022-10-18 19:54:58 -0300

    Add files via upload

---
+++ 016_operador_or/js/scripts.js
@@ -0,0 +1,15 @@
+var nome = 'João';
+var idade = 16;
+
+if(nome == 'João' || idade > 14 ){
+    console.log('Pode fazer aula de esgrima');
+} else {
+    console.log('Não pode fazer aula');
+}
+
+
+if((nome == 'Pedro' || 15 > 20 ) && 10 == 10){
+    console.log('Testando');
+} else {
+    console.log('Não entrou');
+}
---
+++ 017_operador_not/js/scripts.js
@@ -0,0 +1,11 @@
+if(!true){
+    console.log("Passou");
+} else{
+    console.log("Falso");
+}
+
+
+var nome = "Mateus"
+if(!(nome == "João")) {
+    console.log("Ok")
+}
